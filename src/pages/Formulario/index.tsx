70be2408276083f2b2ecc97381c2c3582a27e3d8
pcbandeira-m <pcbandeira.m@gmail.com>
2026-06-09 15:54:34 -0300

    Iniciando o exercicio pratico

--- src/pages/Formulario/index.tsx
+++ src/pages/Formulario/index.tsx
@@ -1,5 +1,16 @@
 import { useForm } from "react-hook-form";
 
+/*
+Exercício Prático:
+Crie um componente de formulário usando o React Hook Form que tenha os campos nome (string), email (string), telefone (string) e idade (number);
+
+As seguintes funcionalidades devem ser implementadas:
+- Limpar todos os campos
+- Submissão de formulário: ao submeter o formulário, é necessário verificar se os campos foram preenchidos. 
+	- Se algum campo estiver vazio, um aviso sobre o preenchimento de todos os campos deve ser exibido no console.
+	- Se todos os campos estiverem preenchidos, uma mensagem de sucesso deve ser exibida no console.
+*/
+
 function Formulario() {
 	const { handleSubmit, register } = useForm();
 	// register é usado para registrar os campos do formulário e precisa ser inserido em seus inputs
@@ -10,32 +21,77 @@ function Formulario() {
 		console.log(data);
 	}
 
+	function resetForm() {
+		// aqui podemos usar a função reset do react-hook-form para limpar os campos do formulário
+	}
+
 	return (
 		<>
-			<div className="container">
+			<div className="container mt-5">
 				<div className="row d-flex justify-content-center">
 					<div className="col-6">
 						<form onSubmit={handleSubmit(dataHandler)}>
 							{/* handleSubmit irá cuidar da submissão em si e dataHandler irá resolver o que fazer com os dados */}
 							<div className="br-input">
-								<label htmlFor="titulo">Título</label>
+								<label htmlFor="name">Nome</label>
 								<input
-									id="titulo"
+									id="name"
 									type="text"
-									placeholder="Insira o título da tarefa"
-									{...register("titulo")}
+									placeholder="Digite seu nome"
+									{...register("name", {
+										required: "True",
+										maxLength: 20,
+									})}
 								/>
 							</div>
-							<div className="br-input">
-								<label htmlFor="descricao">Descrição</label>
+							<div className="br-input mt-3">
+								<label htmlFor="email">Email</label>
 								<input
-									id="descricao"
+									id="email"
 									type="text"
-									placeholder="Insira a descrição da tarefa"
-									{...register("descricao")}
+									placeholder="Digite seu email"
+									{...register("email", {
+										required: "True",
+										pattern: /^\S+@\S+$/i,
+									})}
 								/>
 							</div>
-							<button className="br-button primary">
+							<div className="br-input mt-3">
+								<label htmlFor="phone">Telefone</label>
+								<input
+									id="phone"
+									type="text"
+									placeholder="Digite seu telefone"
+									{...register("phone", {
+										required: "True",
+										pattern:
+											/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/i,
+									})}
+								/>
+							</div>
+							<div className="br-input mt-3">
+								<label htmlFor="age">Qual sua idade?</label>
+								<input
+									id="age"
+									type="number"
+									placeholder="Digite sua idade"
+									{...register("age", {
+										required: "True",
+										min: 0,
+										max: 120,
+									})}
+								/>
+							</div>
+							<button
+								className="br-button secondary mt-4"
+								type="reset"
+							>
+								Limpar
+							</button>
+							<button
+								className="br-button primary mt-4 ml-2"
+								type="submit"
+							>
 								Enviar
 							</button>
 						</form>
