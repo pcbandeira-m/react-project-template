02189c5bf0d1d68d984433c2768bf264fb3fa6d6
pcbandeira-m <pcbandeira.m@gmail.com>
2026-06-09 15:26:14 -0300

    Formulário APÓS uso da biblioteca react hook form

--- src/pages/Formulario/index.tsx
+++ src/pages/Formulario/index.tsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Formulario() {
-	// declarando um estado para cada campo
-	const [titulo, setTitulo] = useState<String>("");
-	const [descricao, setDescricao] = useState<String>("");
+	const { handleSubmit, register } = useForm();
+	// register é usado para registrar os campos do formulário e precisa ser inserido em seus inputs
+	// handleSubmit é usado para lidar com o envio do formulário
 
-	function handleSubmit(e) {
-		e.preventDefault(); // impede o comportamento padrão de recarregamento da página
-
-		const data = {
-			titulo,
-			descricao,
-		};
+	// função para manipular o formulário antes da submissão para sabermos se o valor será pego e enviado para uma API, se vai ser enviado para uma tela etc.
+	function dataHandler(data) {
 		console.log(data);
 	}
 
@@ -20,30 +15,24 @@ function Formulario() {
 			<div className="container">
 				<div className="row d-flex justify-content-center">
 					<div className="col-6">
-						<form onSubmit={handleSubmit}>
+						<form onSubmit={handleSubmit(dataHandler)}>
+							{/* handleSubmit irá cuidar da submissão em si e dataHandler irá resolver o que fazer com os dados */}
 							<div className="br-input">
 								<label htmlFor="titulo">Título</label>
 								<input
 									id="titulo"
 									type="text"
 									placeholder="Insira o título da tarefa"
-									value={titulo}
-									onChange={(e) => {
-										setTitulo(e.target.value);
-									}}
+									{...register("titulo")}
 								/>
 							</div>
-
 							<div className="br-input">
 								<label htmlFor="descricao">Descrição</label>
 								<input
 									id="descricao"
 									type="text"
 									placeholder="Insira a descrição da tarefa"
-									value={descricao}
-									onChange={(e) => {
-										setDescricao(e.target.value);
-									}}
+									{...register("descricao")}
 								/>
 							</div>
 							<button className="br-button primary">
