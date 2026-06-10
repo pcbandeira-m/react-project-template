08ec8c2d4e64b943d5bf06cd384f43114284fb04
pcbandeira-m <pcbandeira.m@gmail.com>
2026-06-10 16:28:54 -0300

    Exercício prático sobre validação de formulários com yup

--- src/pages/Formulario/index.tsx
+++ src/pages/Formulario/index.tsx
@@ -3,13 +3,42 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Link } from "react-router-dom";
 
+/*
+Exercício Prático: Utilize o Yup para realizar a validação do formulário criado no exercício da aula anterior. Crie um esquema de validação dos campos com as regras:
+- nome(string): Campo obrigatório de tamanho mínimo de 3 caracteres.
+- email(string): Campo obrigatório de tamanho máximo de 30 caracteres.
+- telefone(string): Campo opcional de tamanho máximo de 11 caracteres.
+- idade(number): Campo obrigatório de valores positivos.
+
+Ao submeter o formulário, se um campo apresentar erro, a respectiva mensagem deve ser exibida próxima a ele.
+*/
+
 // Declaração do schema para validação dos dados do formulário
 const schema = yup.object().shape({
-	name: yup.string().required("Nome é obrigatório"),
-	email: yup.string().email("Email inválido").required("Email é obrigatório"),
-	phone: yup.string().required("Telefone é obrigatório"),
+	name: yup
+		.string()
+		.required("Nome é obrigatório")
+		.min(3, "Nome deve ter pelo menos 3 caracteres"),
+	email: yup
+		.string()
+		.email("Email inválido")
+		.required("Email é obrigatório")
+		.max(30, "Email deve ter no máximo 30 caracteres"),
+	phone: yup
+		.string()
+		.test(
+			"type",
+			"O telefone deve ser composto apenas de números.",
+			(value) => !value || /^\d+$/.test(value),
+		)
+		.test(
+			"len",
+			"O telefone deve ter exatamente 11 caracteres.",
+			(value) => !value || value.length === 11,
+		),
 	age: yup
 		.number()
+		.transform((value) => (Number.isNaN(value) ? null : value))
 		.positive("A idade deve ser um número positivo")
 		.integer("A idade deve ser um número inteiro")
 		.required("Idade é obrigatória"),
@@ -77,7 +106,7 @@ function Formulario(): JSX.Element {
 							<h3>Formulário de Contato</h3>
 
 							<div
-								className={`br-input mt-3 ${errors.email !== undefined ? "danger" : ""}`} // Código condicional para adicionar a classe "danger" ao input quando houver um erro
+								className={`br-input mt-3 ${errors.name !== undefined ? "danger" : ""}`} // Código condicional para adicionar a classe "danger" ao input quando houver um erro
 							>
 								<label htmlFor="name">Nome</label>
 								<input
@@ -87,7 +116,7 @@ function Formulario(): JSX.Element {
 									{...register("name")}
 								/>
 								{/* Código condicional para a mensagem de erro */}
-								{errors.email !== undefined && (
+								{errors.name !== undefined && (
 									<span
 										className="feedback danger"
 										role="alert"
@@ -98,7 +127,7 @@ function Formulario(): JSX.Element {
 											aria-hidden="true"
 										></i>
 
-										{errors.email?.message}
+										{errors.name?.message}
 									</span>
 								)}
 							</div>
